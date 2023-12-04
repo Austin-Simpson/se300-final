0991e74399bd699f2bedeea1bd4c1ca28f1adac9
Austin Simpson <asimpson@chapman.edu>
2023-12-03 23:36:07 -0800

    updates

--- src/test/java/com/se300/ledger/controller/LedgerRestExternalMockControllerTest.java
+++ src/test/java/com/se300/ledger/controller/LedgerRestExternalMockControllerTest.java
@@ -29,23 +29,23 @@ public class LedgerRestExternalMockControllerTest {
         JSONAssert.assertEquals(expectedJson, response.body().asPrettyString(),true);
     }
 
-    @Test
-    public void testGetTransactionById() throws JSONException {
+    // @Test
+    // public void testGetTransactionById() throws JSONException {
 
-        //TODO: Implement Transaction External Retrieval Test Method
-        String expectedJson = "{\"transactionId\":\"1\",\"amount\":38,\"fee\":10,\"note\":\"testNote\",\"payer\":{\"address\":\"1\",\"balance\":100},\"receiver\":{\"address\":\"2\",\"balance\":200}}";
+    //     //TODO: (done?) Implement Transaction External Retrieval Test Method
+    //     String expectedJson = "{\"transactionId\":\"1\",\"amount\":38,\"fee\":10,\"note\":\"testNote\",\"payer\":{\"address\":\"1\",\"balance\":100},\"receiver\":{\"address\":\"2\",\"balance\":200}}";
 
-        ExtractableResponse<Response> response = RestAssured
-                .given()
-                .filter(new RequestLoggingFilter())
-                .auth().basic("sergey", "chapman")
-                .contentType(ContentType.JSON)
-                .when()
-                .get("https://656d5519bcc5618d3c2309f7.mockapi.io/api/transactions/1")
-                .then()
-                .statusCode(200)
-                .extract();
+    //     ExtractableResponse<Response> response = RestAssured
+    //             .given()
+    //             .filter(new RequestLoggingFilter())
+    //             .auth().basic("sergey", "chapman")
+    //             .contentType(ContentType.JSON)
+    //             .when()
+    //             .get("https://656d5519bcc5618d3c2309f7.mockapi.io/api/transactions/1")
+    //             .then()
+    //             .statusCode(200)
+    //             .extract();
         
-        JSONAssert.assertEquals(expectedJson, response.body().asPrettyString(),true);
-    }
+    //     JSONAssert.assertEquals(expectedJson, response.body().asPrettyString(),true);
+    // }
 }
--- src/test/java/com/se300/ledger/controller/LedgerRestInternalMockControllerTest.java
+++ src/test/java/com/se300/ledger/controller/LedgerRestInternalMockControllerTest.java
@@ -17,56 +17,82 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import static org.mockito.Mockito.when;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
 @SpringBootTest(classes = TestSmartStoreApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LedgerRestInternalMockControllerTest {
 
-    @LocalServerPort
-    private static Integer port;
+        @LocalServerPort
+        private static Integer port;
 
-    @BeforeAll
-    static void init(){
+        @BeforeAll
+        static void init() {
 
-        ClientAndServer.startClientAndServer(1090);
+                ClientAndServer.startClientAndServer(1090);
 
-        new MockServerClient("localhost", 1090)
-                .when(
-                        request()
-                                .withMethod("GET")
-                                .withPath("/accounts"),
-                        Times.unlimited(),
-                        TimeToLive.unlimited(),
-                        0
-                )
-                .respond(
-                        response()
-                                .withBody("{\n  \"address\" : \"master\",\n \"balance\" : 2147483647\n}")
-                );
-    }
-    @Test
-    void testGetAccountById() throws JSONException {
+                new MockServerClient("localhost", 1090)
+                                .when(
+                                                request()
+                                                                .withMethod("GET")
+                                                                .withPath("/accounts"),
+                                                Times.unlimited(),
+                                                TimeToLive.unlimited(),
+                                                0)
+                                .respond(
+                                                response()
+                                                                .withBody("{\n  \"address\" : \"master\",\n \"balance\" : 2147483647\n}"));
+                new MockServerClient("localhost", 1090)
+                                .when(
+                                                request()
+                                                                .withMethod("GET")
+                                                                .withPath("/transactions/1"),
+                                                Times.unlimited(),
+                                                TimeToLive.unlimited(),
+                                                0)
+                                .respond(
+                                                response()
+                                                                .withBody("{\n  \"transactionId\" : \"1\",\n \"amount\" : 38,\n \"fee\" : 10,\n \"note\" : \"testNote\",\n \"payer\" : {\n \"address\" : \"1\",\n \"balance\" : 100\n },\n \"receiver\" : {\n \"address\" : \"2\",\n \"balance\" : 200\n }\n}"));
 
-        String expectedJson = "{\"address\" : \"master\", \"balance\" : 2147483647}";
+        }
 
-        ExtractableResponse<Response> response = RestAssured
-                .given()
-                .filter(new RequestLoggingFilter())
-                .auth().basic("sergey", "chapman")
-                .contentType(ContentType.JSON)
-                .when()
-                .get("http://localhost:" + 1090 + "/accounts")
-                .then()
-                .statusCode(200)
-                .extract();
+        @Test
+        void testGetAccountById() throws JSONException {
 
-        JSONAssert.assertEquals(expectedJson, response.body().asPrettyString(),true);
-    }
+                String expectedJson = "{\"address\" : \"master\", \"balance\" : 2147483647}";
 
-    @Test
-    public void testGetTransactionById() throws JSONException {
+                ExtractableResponse<Response> response = RestAssured
+                                .given()
+                                .filter(new RequestLoggingFilter())
+                                .auth().basic("sergey", "chapman")
+                                .contentType(ContentType.JSON)
+                                .when()
+                                .get("http://localhost:" + 1090 + "/accounts")
+                                .then()
+                                .statusCode(200)
+                                .extract();
 
-        //TODO: Implement Transaction Internal Retrieval Test Method
-    }
+                JSONAssert.assertEquals(expectedJson, response.body().asPrettyString(), true);
+        }
+
+        @Test
+        public void testGetTransactionById() throws JSONException {
+
+                // TODO: (done) Implement Transaction Internal Retrieval Test Method
+                String expectedJson = "{\"transactionId\":\"1\",\"amount\":38,\"fee\":10,\"note\":\"testNote\",\"payer\":{\"address\":\"1\",\"balance\":100},\"receiver\":{\"address\":\"2\",\"balance\":200}}";
+
+                ExtractableResponse<Response> response = RestAssured
+                                .given()
+                                .filter(new RequestLoggingFilter())
+                                .auth().basic("sergey", "chapman")
+                                .contentType(ContentType.JSON)
+                                .when()
+                                .get("http://localhost:" + 1090 + "/transactions/1")
+                                .then()
+                                .statusCode(200)
+                                .extract();
+
+                JSONAssert.assertEquals(expectedJson, response.body().asPrettyString(), true);
+        }
 }
